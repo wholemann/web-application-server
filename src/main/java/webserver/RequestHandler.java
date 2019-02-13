1828db465984aa775f5dcbcfc6ecd52d138fb01a
wholeman <gowgow0606@naver.com>
2019-02-13 16:46:31 +0900

    중복 메소드 리팩토링

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
-import javax.xml.crypto.Data;
-
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -31,7 +29,6 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-
             /*
             1. 먼저 user의 http request를 서버쪽에서 읽어온다.
             2. 요청된 url을 추출한다.
@@ -49,7 +46,7 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
             int contentLength = 0;
-            String requestHeaderCookie = null;
+            boolean isLogin = false;
 
             while (!line.equals("")) {
                 line = br.readLine();
@@ -59,7 +56,7 @@ public class RequestHandler extends Thread {
                 }
 
                 if (line.contains("Cookie")) {
-                    requestHeaderCookie = getCookie(line);
+                    isLogin = isLogin(line);
                 }
 
                 if (line.contains("text/css")) {
@@ -82,10 +79,8 @@ public class RequestHandler extends Thread {
 
                 DataBase.addUser(user);
                 log.debug("User : {}", user);
-                byte[] body = Files.readAllBytes(new File("./webapp" + "/index.html").toPath());
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos, "/index.html");
-                responseBody(dos, body);
 
             } else if (requestedUrl.equals("/user/login")) {
                 log.debug("로그인");
@@ -94,76 +89,46 @@ public class RequestHandler extends Thread {
                 log.debug(requestBody);
 
                 User user = DataBase.findUserById(params.get("userId"));
-                // 해당 유저가 가입된 경우
-                if (user != null) {
-                    log.debug("가입된 유저 : " + user.getUserId());
-                    //비밀 번호가 일치하는 경우
-                    if (user.getPassword().equals(params.get("password"))) {
-                        log.debug("로그인 성공");
-                        byte[] body = Files.readAllBytes(new File("./webapp" + "/index.html").toPath());
-                        DataOutputStream dos = new DataOutputStream(out);
-                        responseLogin(dos, body.length, true);
-                        responseBody(dos, body);
-                        // 비밀 번호가 일치하지 않는 경우
-                    } else {
-                        log.debug("로그인 실패");
-                        byte[] body = Files.readAllBytes(new File("./webapp" + "/user/login_failed.html").toPath());
-                        DataOutputStream dos = new DataOutputStream(out);
-                        responseLogin(dos, body.length, false);
-                        responseBody(dos, body);
-                    }
-                    // 해당 유저가 DB에 없는 경우
-                } else {
+
+                if (user == null) {
                     log.debug("가입되지 않은 유저");
-                    byte[] body = Files.readAllBytes(new File("./webapp" + "/user/login_failed.html").toPath());
+                    responseResource(out, "/user/login_failed.html");
+                    return;
+                }
+                // 해당 유저가 가입된 경우
+                if (user.getPassword().equals(params.get("password"))) {
                     DataOutputStream dos = new DataOutputStream(out);
-                    responseLogin(dos, body.length, false);
-                    responseBody(dos, body);
+                    response302LoginSuccessHeader(dos);
+                } else {
+                // 비밀번호가 틀린 경우
+                    responseResource(out, "/user/login_failed.html");
                 }
 
             } else if (requestedUrl.equals("/user/list.html")) {
                 log.debug("사용자 목록 보여주기");
-                log.debug(requestHeaderCookie);
 
-                if (requestHeaderCookie != null) {
-                    Map<String, String> cookieParams = HttpRequestUtils.parseCookies(requestHeaderCookie);
-                    String logined = cookieParams.get("logined");
-                    if (Boolean.parseBoolean(logined)) {
-                        Collection<User> userList = DataBase.findAll();
-                        StringBuilder sb = new StringBuilder();
-                        sb.append("<table border='1'>");
-                        for (User user : userList) {
-                            sb.append("<tr>");
-                            sb.append("<td>" + user.getUserId() + "</td>");
-                            sb.append("<td>" + user.getName() + "</td>");
-                            sb.append("<td>" + user.getEmail() + "</td>");
-                            sb.append("</tr>");
-                        }
-                        sb.append("</table>");
-                        byte[] body = sb.toString().getBytes();
-                        DataOutputStream dos = new DataOutputStream(out);
-                        response200Header(dos, body.length);
-                        responseBody(dos, body);
-                    } else {
-                        log.debug("로그인 되지 않은 사용자");
-                        byte[] body = Files.readAllBytes(new File("./webapp" + "/user/login.html").toPath());
-                        DataOutputStream dos = new DataOutputStream(out);
-                        response302Header(dos, "/user/login.html");
-                        responseBody(dos, body);
-                    }
-
-                } else {
-                    byte[] body = Files.readAllBytes(new File("./webapp" + "/user/login.html").toPath());
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response302Header(dos, "/user/login.html");
-                    responseBody(dos, body);
+                if (!isLogin) {
+                    responseResource(out, "/user/login.html");
+                    return;
                 }
-
-            } else {
-                byte[] body = Files.readAllBytes(new File("./webapp" + requestedUrl).toPath());
+                Collection<User> userList = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : userList) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
                 DataOutputStream dos = new DataOutputStream(out);
                 response200Header(dos, body.length);
                 responseBody(dos, body);
+
+            } else {
+                responseResource(out, requestedUrl);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -175,9 +140,14 @@ public class RequestHandler extends Thread {
         return Integer.parseInt(headerTokens[1].trim());
     }
 
-    private String getCookie(String line) {
+    private boolean isLogin(String line) {
         String[] headerTokens = line.split(":");
-        return headerTokens[1].trim();
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -199,33 +169,24 @@ public class RequestHandler extends Thread {
         try {
             dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
             dos.writeBytes("Location: " + url + "\r\n");
-            if (includeCSS) {
-                dos.writeBytes("Content-Type: text/css\r\n");
-            } else {
-                dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            }//            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error("response302Header: " + e.getMessage());
         }
     }
 
-    private void responseLogin(DataOutputStream dos, int lengthOfBodyContent, boolean loginSuccess) {
+    private void responseResource(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void response302LoginSuccessHeader(DataOutputStream dos) {
         try {
             dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
-            if (includeCSS) {
-                dos.writeBytes("Content-Type: text/css\r\n");
-            } else {
-                dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            }
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            if (loginSuccess) {
-                dos.writeBytes("Location: /index.html\n");
-                dos.writeBytes("Set-Cookie: logined=" + String.valueOf(loginSuccess) + "\r\n");
-            } else {
-                dos.writeBytes("Location: /user/login_failed.html\n");
-                dos.writeBytes("Set-Cookie: logined=" + String.valueOf(loginSuccess) + "\r\n");
-            }
+            dos.writeBytes("Set-Cookie: logined=true \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error("responseSetCookieHeader: " + e.getMessage());
